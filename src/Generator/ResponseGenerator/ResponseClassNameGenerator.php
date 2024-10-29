7be30df2975cdc0d8f664402b5e139a5b3341970
Christoph Hochholzer <christoph@hochholzer.info>
2024-10-29 21:19:50 +0100

    Enhance ResponseClassNameGenerator by case mapping

--- src/Generator/ResponseGenerator/ResponseClassNameGenerator.php
+++ src/Generator/ResponseGenerator/ResponseClassNameGenerator.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Xenos\OpenApiClientGenerator\Generator\ResponseGenerator;
 
+use Error;
+use InvalidArgumentException;
+use Xenos\OpenApi\Model\AbstractComponentsSubList;
 use Xenos\OpenApi\Model\Operation;
 use Xenos\OpenApiClientGenerator\Generator\Config\Config;
 use Xenos\OpenApiClientGenerator\Model\FullyQualifiedClassName;
@@ -12,7 +15,11 @@ use function array_map;
 use function array_pop;
 use function explode;
 use function implode;
+use function is_null;
+use function preg_match;
+use function preg_replace_callback;
 use function strtolower;
+use function strtoupper;
 use function ucfirst;
 
 class ResponseClassNameGenerator
@@ -42,7 +49,22 @@ class ResponseClassNameGenerator
 
     public function createResponseClassNameFromComponentsKey(string $componentsKey): FullyQualifiedClassName
     {
-        return new FullyQualifiedClassName($this->config->namespace . '\Response\\' . ucfirst($componentsKey));
+        if (!preg_match(AbstractComponentsSubList::KEY_PATTERN, $componentsKey)) {
+            throw new InvalidArgumentException('Component key must be a string matching the regular expression "^[a-zA-Z0-9._-]+$", ' . var_export($componentsKey, true) . ' given.');
+        }
+
+        // Removes some special chars which are allowed in component keys and sets the following char to upper case
+        $className = preg_replace_callback(
+            pattern: '/[.\-_](.)/',
+            callback: fn (array $matches): string => strtoupper($matches[1]),
+            subject: $componentsKey
+        );
+
+        if (is_null($className)) {
+            throw new Error();
+        }
+
+        return new FullyQualifiedClassName($this->config->namespace . '\Response\\' . ucfirst($className));
     }
 
     private function convertEndpointToCamelCase(string $endpoint): string
---
+++ tests/src/Generator/ResponseGenerator/ResponseClassNameGeneratorTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xenos\OpenApiClientGeneratorTest\Generator\ResponseGenerator;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Xenos\OpenApiClientGenerator\Generator\Config\Config;
+use Xenos\OpenApiClientGenerator\Generator\ResponseGenerator\ResponseClassNameGenerator;
+
+use function var_export;
+
+class ResponseClassNameGeneratorTest extends TestCase
+{
+    private ResponseClassNameGenerator $responseClassNameGenerator;
+
+    protected function setUp(): void
+    {
+        $this->responseClassNameGenerator = new ResponseClassNameGenerator(
+            new Config('PetShop', '/test')
+        );
+    }
+
+    #[DataProvider('provideComponentNamesAndClassNames')]
+    public function testCreateResponseClassNameFromComponentsKey(
+        string $componentsKey,
+        string $expectedClassName,
+    ): void {
+        self::assertSame(
+            $expectedClassName,
+            (string)$this->responseClassNameGenerator->createResponseClassNameFromComponentsKey($componentsKey)
+        );
+        self::assertMatchesRegularExpression(
+            pattern: '/^[a-zA-Z_\\x80-\\xff][a-zA-Z0-9_\\x80-\\xff]*(\\\\[a-zA-Z_\\x80-\\xff][a-zA-Z0-9_\\x80-\\xff]*)*$/',
+            string: $expectedClassName,
+            message: ResponseClassNameGenerator::class . ' generated an invalid class name',
+        );
+    }
+
+    public static function provideComponentNamesAndClassNames(): array
+    {
+        return [
+            'one word class name upper case' => ['Pet', 'PetShop\Response\Pet'],
+            'one word class name lower case' => ['pet', 'PetShop\Response\Pet'],
+            'one word class name lower case with number in the end' => ['pet123', 'PetShop\Response\Pet123'],
+            'lower dot case' => ['pet.shop', 'PetShop\Response\PetShop'],
+            'lower kebap case' => ['class-name', 'PetShop\Response\ClassName'],
+            'lower snake case' => ['test_class', 'PetShop\Response\TestClass'],
+            ['my-dog.pet_shop', 'PetShop\Response\MyDogPetShop'],
+            ['.pet', 'PetShop\Response\Pet'],
+            ['_pet', 'PetShop\Response\Pet'],
+            ['-pet', 'PetShop\Response\Pet']
+        ];
+    }
+
+    #[DataProvider('provideInvalidComponentKeys')]
+    public function testCreateResponseClassNameFromComponentsKeyThrowsInvalidArgumentException(
+        string $componentsKey,
+    ): void {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Component key must be a string matching the regular expression "^[a-zA-Z0-9._-]+$", ' . var_export($componentsKey, true) . ' given.');
+
+        $this->responseClassNameGenerator->createResponseClassNameFromComponentsKey($componentsKey);
+    }
+
+    public static function provideInvalidComponentKeys(): array
+    {
+        return [
+            ['two words'],
+            ['@'],
+            ['#'],
+            ['%'],
+            [PHP_EOL],
+            ['ä'],
+            ['ö'],
+            ['ü'],
+            ['ß'],
+        ];
+    }
+}
